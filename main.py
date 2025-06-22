b6dd9f4216cc5fd8caf19bd1816de45fc55c42bc
codewizadrishit <codewizardrishit@gmail.com>
2025-06-22 13:15:53 +0530

    Added logic and  functionality

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from pygame.locals import *
 # Global variables
 FPS = 32
 SCREENWIDTH = 350
-SCREENHEIGHT = 690
+SCREENHEIGHT = 630
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-GROUNDY = SCREENHEIGHT * 0.8  # 80% of the screen height
+GROUNDY = SCREENHEIGHT * 0.85  
 GAME_SPRITES = {}
 GAME_SOUNDS = {}
 PLAYER = 'gallery/sprites/bird.png'
@@ -43,7 +43,139 @@ def mainGame():
     score = 0
     playerx = int(SCREENWIDTH/5)
     playery = int(SCREENWIDTH/2)
+    basex = 0
 
+    newPipe1 = getRandomPipe()
+    newPipe2 = getRandomPipe()
+
+    upperPipes = [
+        {'x': SCREENWIDTH+200, 'y': newPipe1[0]['y']},
+        {'x': SCREENWIDTH+200+SCREENWIDTH/2, 'y': newPipe2[0]['y']}
+    ]
+
+    lowerPipes = [
+        {'x': SCREENWIDTH+200, 'y': newPipe1[1]['y']},
+        {'x': SCREENWIDTH+200+SCREENWIDTH/2, 'y': newPipe2[1]['y']}
+    ]
+
+    pipeVelX = -4
+    playerVelY = -9
+    playerMaxVelY = 10
+    playerMinVelY = -8
+    playerAccY = 1
+    playerFlapAccV = -8  # velocity while flapping
+    playerFlapped = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
+                if playery > 0:
+                    playerVelY = playerFlapAccV
+                    playerFlapped = True
+                    GAME_SOUNDS['wing'].play()
+
+        crashTest = isCollide(playerx, playery, upperPipes, lowerPipes)
+        if crashTest:
+            return
+
+        # Score Check
+        playerMidPos = playerx + GAME_SPRITES['player'].get_width()/2
+        for pipe in upperPipes:
+            pipeMidPos = pipe['x'] + GAME_SPRITES['pipe'][0].get_width()/2
+            if pipeMidPos <= playerMidPos < pipeMidPos+4:
+                score += 1
+                print(f"Your score is : {score}")
+                GAME_SOUNDS['point'].play()
+
+        if playerVelY < playerMaxVelY and not playerFlapped:
+            playerVelY += playerAccY
+
+        if playerFlapped:
+            playerFlapped = False
+
+        playerHeight = GAME_SPRITES['player'].get_height()
+        playery = playery + min(playerVelY, GROUNDY - playery - playerHeight)
+
+        for upperPipe, lowerPipe in zip(upperPipes, lowerPipes):
+            upperPipe['x'] += pipeVelX
+            lowerPipe['x'] += pipeVelX
+
+        if 0 < upperPipes[0]['x'] < 5:
+            newPipe = getRandomPipe()
+            upperPipes.append(newPipe[0])
+            lowerPipes.append(newPipe[1])
+
+        # if pipe out of screen , remove it
+        if upperPipes[0]['x'] < -GAME_SPRITES['pipe'][0].get_width():
+            upperPipes.pop(0)
+            lowerPipes.pop(0)
+
+        SCREEN.blit(GAME_SPRITES['background'], (0, 0))
+        for upperPipe, lowerPipe in zip(upperPipes, lowerPipes):
+            SCREEN.blit(GAME_SPRITES['pipe'][0],
+                        (upperPipe['x'], upperPipe['y']))
+            SCREEN.blit(GAME_SPRITES['pipe'][1],
+                        (lowerPipe['x'], lowerPipe['y']))
+
+        SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))
+        SCREEN.blit(GAME_SPRITES['player'], (playerx, playery))
+
+        myDigits = [int(x) for x in list(str(score))]
+        DIGIT_WIDTH = 30   
+        DIGIT_HEIGHT = 40
+        width = len(myDigits) * DIGIT_WIDTH
+        
+        
+
+        Xoffset = (SCREENWIDTH-width)/2
+        tempX = Xoffset
+        for digit in myDigits:
+            scaled_digit = pygame.transform.scale(GAME_SPRITES['numbers'][digit],(DIGIT_WIDTH,DIGIT_HEIGHT))
+            SCREEN.blit(scaled_digit,(tempX, SCREENHEIGHT*0.12))
+            tempX += DIGIT_WIDTH
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def isCollide(playerx, playery, upperPipes, lowerPipes):
+    if playery>GROUNDY -30 or playery<0:
+        GAME_SOUNDS['hit'].play()
+        return True
+    pipeHeight = GAME_SPRITES['pipe'][0].get_height()
+
+    for pipe in upperPipes:
+        if (playery < (pipeHeight+pipe['y']) and abs(playerx-pipe['x']) <GAME_SPRITES['pipe'][0].get_width()) :
+            GAME_SOUNDS['hit'].play()
+            return True    
+
+    for pipe in lowerPipes :
+        if (playery + GAME_SPRITES['player'].get_height()> pipe['y'] and abs(playerx-pipe['x']) <GAME_SPRITES['pipe'][0].get_width()) :
+            GAME_SOUNDS['hit'].play()
+            return True
+    return False 
+
+def getRandomPipe():
+    pipeHeight = GAME_SPRITES['pipe'][0].get_height()
+    baseHeight = GAME_SPRITES['base'].get_height()
+
+    gap = int(SCREENHEIGHT / 3.8)  # Reasonable gap between pipes
+    pipeX = SCREENWIDTH + 10
+
+    # Set limits for the Y of the upper pipe (top pipe should not come too low)
+    upperY_min = -pipeHeight + 100  # pipe can be this low at max (i.e., 100 px from top)
+    upperY_max = -pipeHeight + 200  # pipe stays within top quarter of screen
+
+    y1 = random.randint(upperY_min, upperY_max)  # upper pipe Y (always negative)
+    y2 = y1 + pipeHeight + gap  # lower pipe Y, based on upper pipe and gap
+
+    pipe = [
+        {'x': pipeX, 'y': y1},  # Upper pipe
+        {'x': pipeX, 'y': y2}   # Lower pipe
+    ]
+    return pipe
 
 if __name__ == "__main__":
     pygame.init()  # Initializes all pygame modules
